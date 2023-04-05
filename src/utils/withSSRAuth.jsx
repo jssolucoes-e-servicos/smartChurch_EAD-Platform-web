4a77a245d2593630a6fa62c99fd972cdb8de5bbd
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2023-04-05 10:48:13 -0300

    adjust in ssr

--- src/utils/withSSRAuth.jsx
+++ src/utils/withSSRAuth.jsx
@@ -1,16 +1,14 @@
 import { destroyCookie, parseCookies } from "nookies";
 
+
 export function withSSRAuth(ctx) {
   const { "SEAD-00": token } = parseCookies(ctx);
-  if (!token) {
+  if (token === undefined) {
+
     destroyCookie(ctx, "SEAD-00");
     destroyCookie(ctx, "SEAD-01");
     destroyCookie(ctx, "SEAD-02");
-    return {
-      redirect: {
-        destination: "/acesso",
-        permanent: false,
-      },
-    };
+    ctx.res.writeHead(302, { Location: '/acesso' });
+    ctx.res.end();
   }
 }
