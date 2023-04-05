0048490113e5b3fbba035ab0533e4d085f167f35
JS Soluções e Serviços <49538838+jssolucoes-e-servicos@users.noreply.github.com>
2023-04-05 10:42:48 -0300

    create component - pagination

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import {
+  Pagination, PaginationItem, PaginationLink
+} from "reactstrap";
+
+export default function PaginationComponent(props) {
+  return (
+    <Pagination aria-label="Page navigation example">
+      <PaginationItem disabled>
+        <PaginationLink
+          first
+          href="#"
+        />
+      </PaginationItem>
+      <PaginationItem disabled>
+        <PaginationLink
+          href="#"
+          previous
+        />
+      </PaginationItem>
+      <PaginationItem active>
+        <PaginationLink href="#">
+          1
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          2
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem disabled>
+        <PaginationLink href="#">
+          3
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          4
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          5
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink
+          href="#"
+          next
+        />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink
+          href="#"
+          last
+        />
+      </PaginationItem>
+    </Pagination>
+  );
+}
