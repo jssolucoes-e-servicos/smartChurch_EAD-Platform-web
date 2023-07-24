4083926ed8e5f4bbd8ab1b6cd7fbf9be14ab9e4b
JS Soluções e Serviços <jssolucoeseservicos@gmail.com>
2023-07-24 20:49:51 -0300

    add onesignal

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from '~/contexts/AuthContext';
 import '../styles/ReactToastify.css';
@@ -9,14 +9,23 @@ import "~/assets/css/nextjs-argon-dashboard.css";
 import "~/assets/plugins/nucleo/css/nucleo.css";
 
 export default function MyApp({ Component, pageProps }) {
-  /* 
-    useEffect(() => {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-          .register('/pwabuilder-sw.js')
-          .then((registration) => console.log('scope is: ', registration.scope));
-      }
-    }, []); */
+
+  useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "aa8bc1e7-4616-4f17-adc7-740af7a1628e",
+        //safari_web_id: "web.onesignal.auto.364542e4-0165-4e49-b6eb-0136f3f4eaa9",
+        notifyButton: {
+          enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true,
+      });
+    });
+    return () => {
+      window.OneSignal = undefined;
+    };
+  }, []);
 
   return (<AuthProvider>
     <Head>
