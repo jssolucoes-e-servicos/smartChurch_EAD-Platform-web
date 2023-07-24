71022d6fa6e770b87b71855d3babde9de24a22b6
JS Soluções e Serviços <jssolucoeseservicos@gmail.com>
2023-07-24 17:19:26 -0300

    included manifest and sw

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,72 +1,44 @@
-import React from "react";
-//import ReactDOM from "react-dom";
-import App from "next/app";
 import Head from "next/head";
-//import Router from "next/router";
+import React, { useEffect } from "react";
 import { ToastContainer } from 'react-toastify';
-import '../styles/ReactToastify.css';
 import { AuthProvider } from '~/contexts/AuthContext';
+import '../styles/ReactToastify.css';
 
-//import PageChange from "~/components/PageChange";
-
-import "~/assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "~/assets/css/nextjs-argon-dashboard.css";
+import "~/assets/plugins/nucleo/css/nucleo.css";
 
-/* Router.events.on("routeChangeStart", () => {
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(<PageChange />,
-    document.getElementById("page-transition")
-  );
-});
-Router.events.on("routeChangeComplete", () => {
-  //ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  //document.body.classList.remove("body-page-transition");
-});
-Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-}); */
+export default function MyApp({ Component, pageProps }) {
 
-export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment("");
-    document.insertBefore(comment, document.documentElement);
-  }
-
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/pwabuilder-sw.js')
+        .then((registration) => console.log('scope is: ', registration.scope));
     }
+  }, []);
 
-    return { pageProps };
-  }
+  return (<AuthProvider>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <title>smartEAD</title>
+    </Head>
+    <Component {...pageProps} />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  </AuthProvider>)
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <AuthProvider>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>smartEAD</title>
-        </Head>
-        <Component {...pageProps} />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </AuthProvider>
-    );
-  }
+
+
 }
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,7 +1,10 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+
+
+
   render() {
     return (
       <Html lang="pt-br">
@@ -9,11 +12,13 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href="/assets/img/brand/favicon.ico"></link>
+          <link rel="manifest" href="/manifest.json" />
           {/*  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/brand/apple-icon.png" ></link> */}
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
             rel="stylesheet"
           ></link>
+
         </Head>
         <body>
           <div id="page-transition"></div>
